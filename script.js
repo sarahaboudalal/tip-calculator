627d19ade74e0f1026fdb3d6e4c743b671328c14
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-08-31 14:35:29 +0300

    clean up

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const tipButtons = document.querySelectorAll(".btns");
-// console.log(tipButtons)
 const billInput = document.querySelector('.bill-input')
 const customTip = document.querySelector('.custom')
 const peopleInput = document.querySelector('.people-input')
@@ -7,45 +6,54 @@ const tipAmount = document.querySelector('.tip-amount')
 const totalAmount = document.querySelector('.total-amount')
 const resetButton = document.querySelector('.reset-button')
 
-// console.log(customTip)
+
 
 function calculate(total) {
+
 let tipPerPerson = (total * billInput.value)/ 100 / peopleInput.value;
 let totalPerPerson = (billInput.value / peopleInput.value) + tipPerPerson;
-//    console.log(totalPerPerson)
-//    console.log(tipPerPerson)
 tipAmount.innerHTML ="$" + tipPerPerson.toFixed(2)
 totalAmount.innerHTML = "$" + totalPerPerson.toFixed(2)
 
 }
 
 function reset (){
+
     totalAmount.innerHTML = "$0.00"
     tipAmount.innerHTML ="$0.00"
     billInput.value = "";
     peopleInput.value = "";
     customTip.value = "";
+
 }
+
 peopleInput.addEventListener("input", (e) => {
+
     if(peopleInput.value < 1) {
         alert("Cannot Be Zero!")
         reset()
     }
 })
+
 tipButtons.forEach((button) => {
+
     button.addEventListener("click", (e) => {
        let total = parseInt(e.target.value);
      calculate(total);
+
     })
 })
 
 customTip.addEventListener("input", (e) => {
-//   console.log(e.target.value)
+
 let total = e.target.value;
 calculate(total)
+
 })
 
 resetButton.addEventListener("click", (e) => {
+
     reset()
+    
 })
 
